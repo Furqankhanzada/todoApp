e470ed31e7a6d2beb706d7f508d5f021e4ab2e1f
Furqankhanzada <furqan.khanzada@gmail.com>
2013-02-07 00:08:09 +0500

    commit for deploy to heroku

--- web.js
+++ web.js
@@ -47,7 +47,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
